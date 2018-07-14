77c5ebdcd290893d190abb88b34dbac3f4fc5743
JakubGajewski <gajewski_jakub@yahoo.pl>
2018-07-14 15:40:19 +0200

    7 KYU Java with Java8

--- src/pl/_7kyu/HighestAndLowest.java
+++ src/pl/_7kyu/HighestAndLowest.java
@@ -1,9 +1,39 @@
+//http://www.codewars.com/kata/554b4ac871d6813a03000035/solutions/java
+
 package pl._7kyu;
 
 import java.util.*;
 
 public class HighestAndLowest {
     public static String HighAndLow(String numbers){
+
+        List <String> numberList = Arrays.asList(numbers.split(" "));
+
+        Integer min = numberList.stream()
+                .map(s -> Integer.parseInt(s))
+                .min(new Comparator<Integer>() {
+                    @Override
+                    public int compare(Integer o1, Integer o2) {
+                        return Integer.compare(o1,o2);
+                    }
+                })
+                .orElse(Integer.valueOf(numberList.get(0)));
+
+        int max = numberList.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .max()
+                .getAsInt();
+
+        return (max + " " + min);
+        }
+}
+
+
+
+
+
+
+/*My first solution:
         List <String> numberList = Arrays.asList(numbers.split(" "));
         Integer min = numberList.stream()
                 .map(s -> Integer.parseInt(s))
@@ -23,6 +53,4 @@ public class HighestAndLowest {
                     }
                 })
                 .orElse(Integer.valueOf(numberList.get(0)));
-        return (max + " " + min);
-    };
-}
+        return (max + " " + min);*/
